678bb4a481a413cf723185d739f304291981e527
emilieturmine <emilie.turmine@gmail.com>
2022-05-06 11:51:33 +0200

    back  06052022

---
+++ BackEnd/PHP/index.php
@@ -0,0 +1,7 @@
+<?php
+    $calendrier = array("Janvier" => 31,"Fevrier" => 29, "Mars" => 31,"Avril" => 30,"Mai" => 31,"Juin" => 30,"Juillet" => 31,"Aout" => 31,"Septembre" => 30,"Octobre" => 31,  
+"Novembre" => 30,"Decembre" => 31); 
+
+    foreach ($calendrier as $mois => $valeur){ 
+       echo " Le mois de $mois : $valeur Jours <br />";
+           } 
